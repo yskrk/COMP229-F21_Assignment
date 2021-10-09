2fba14fdc1722d3ff8651dd801fb1ea39199392d
Yusuke <ykuroki@my.centennialcollege.ca>
2021-10-09 03:15:25 -0400

    update db uri to work with atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,5 @@
+// db for localhost
 module.exports = {
-	"URI": "mongodb://localhost/book_store"
+	// "URI": "mongodb://localhost/book_store"
+	"URI": "mongodb+srv://yusuke:x8uInaUwHyqUq1md@mongodbserver.c8grk.mongodb.net/comp229f21?retryWrites=true&w=majority"
 }
