2154d9ad11aaddca9f032108880fab2d93b2d149
Yusuke <ykuroki@my.centennialcollege.ca>
2021-10-09 15:33:51 -0400

    week 6 commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,10 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let passport = require('passport');
 
+// enable jwt
+let jwt = require('jsonwebtoken');
+let DB = require('../config/db');
+
 // create the User Model instance
 let userModel = require('../models/user');
 let User = userModel.User;	// alias
@@ -60,6 +64,29 @@ module.exports.processLoginPage = (req, res, next) => {
 				if (err) {
 					return next(err);
 				}
+
+				const payload = {
+					id: user._id,
+					displayName: user.displayname,
+					usernamer: user.username,
+					email: user.email
+				}
+
+				const authToken = jwt.sign(payload, DB.Secret, 
+					{
+						expiresIn: 604800 // 1 week
+					});
+
+					// TODO - getting ready to convert to API
+					// res.json({success: true, msg: 'User Logged in Successfully', user: 
+					// 	{
+					// 		id: user._id,
+					// 		displayName: user.displayname,
+					// 		usernamer: user.username,
+					// 		email: user.email
+					// 	}, 
+					// 	token: authToken});
+
 				return res.redirect('/book-list');
 			});
 		}
@@ -110,6 +137,10 @@ module.exports.processRegisterPage = (req, res, next) => {
 			// if no error exists, then registration is successful
 
 			// redirect the user and authenticate them
+
+			// TODO - getting ready to convert to API
+			// res.json({success: true, msg: 'User Registered Successfully!'});
+
 			return passport.authenticate('local')(req, res, () => {
 				res.redirect('/book-list');
 			});
